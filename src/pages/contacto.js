7bfa605c92ab7a696ac7e8a5de06e16b5d002f66
La Diega <83097524+Ladiega@users.noreply.github.com>
2023-02-13 11:16:59 -0500

    update pages contacto and horarios

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,9 +1,14 @@
+import Sticky from "components/sticky/sticky"
+import Footer from "components/footer/index"
+
 export default function Contacto(){
     return(
         <div>
+					<Sticky/>
 					<section>
 						CONTACTO
 					</section>
+					<Footer/>
 				</div>
     )
 }
--- src/pages/horarios.js
+++ src/pages/horarios.js
@@ -1,11 +1,15 @@
 import Sticky from "components/sticky/sticky"
 import Footer from 'components/footer/index'
-import style from '../styles/Home.module.css'
+
+import styles from '../styles/Horarios.module.css'
 export default function Horarios(){
     return(
-        <div className={style.global_contaner}>
+        <div className={styles.horarios_container}>
             <Sticky/>
-            Horarios
+            <div className={styles.content_container}>
+                <h1>HORARIOS</h1>
+                
+            </div>
             
             <Footer/>
 
