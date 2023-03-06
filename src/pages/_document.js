fed63ef44c068f2be276f8ff473b8d5658483615
ladiega <fokuiatodo@gmail.com>
2023-03-06 17:50:45 -0500

    adding inbox chat on _document.js Head script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+     
+  <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/24195439.js"></script>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
